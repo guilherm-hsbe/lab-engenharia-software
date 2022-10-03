7457ee519d55d8304b01e8d31c96324e9d8f5325
Guilherme HSBE <guilherm3hsb@gmail.com>
2022-10-02 22:32:57 -0300

    *started

--- react/projetos_react/letreiro_react/src/App.js
+++ react/projetos_react/letreiro_react/src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+var string = "Venha Estudar na FATEC...";
+var vazio = "";
+var i = 0;
+
 function App() {
+  const element = (<h4>{vazio += string[i]}</h4>);
+  i++;
+
+  if (i <= string.length){
+      ReactDOM.render(element,document.getElementById('root'));
+  }
+
+  setInterval(tick,50);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Letreiro.</p>
+        <p id="root"></p>
       </header>
     </div>
   );
