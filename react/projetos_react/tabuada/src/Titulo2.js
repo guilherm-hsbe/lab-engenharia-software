364901f546722e4f34e2a3d02d5a6f7adecf2060
guilherm-hsbe <guilherm3hsb@gmail.com>
2022-10-17 22:00:35 -0300

    *h2

--- react/projetos_react/tabuada/src/Titulo2.js
+++ react/projetos_react/tabuada/src/Titulo2.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Titulo2() {
-    return <h2>Componente de Título com função!</h2>;
+    return <h2>Este é um componente de Título com função!</h2>;
 }
 
 export default Titulo2;
