6ad3a3807f332332c94465763d93db85da7dd7ce
guilherm-hsbe <guilherm3hsb@gmail.com>
2022-11-21 21:24:18 -0300

    *

--- react/projetos_react/projetoloja/loja-api/src/index.js
+++ react/projetos_react/projetoloja/loja-api/src/index.js
@@ -1,3 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// ORIGINAL
 require("dotenv/config");
 const app = require("./app");
 
@@ -5,3 +12,17 @@ const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
   console.log(`API:${PORT}`);
 });
+// ----------
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
