fe61abd781f83bd7b3cfa038e3bda3df736a0714
guilherm-hsbe <guilherm3hsb@gmail.com>
2022-11-28 19:42:29 -0300

    *

--- react/projetos_react/ploja/loja-api/src/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const products = require("./controllers/products");
-const purchases = require("./controllers/purchases");
-
-const router = express.Router();
-router.get("/products", products.select);
-router.post("/purchases", purchases.create);
-
-module.exports = router;
